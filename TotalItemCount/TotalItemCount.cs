9c94682b0f470a85a0a75622f79417bd6da596e2
Triton Pitassi <pitassi17@up.edu>
2020-09-05 19:24:27 -0400

    Added Item Count to Money Text Object

--- TotalItemCount/TotalItemCount.cs
+++ TotalItemCount/TotalItemCount.cs
@@ -1,6 +1,6 @@
 ﻿using BepInEx;
+using R2API.Utils;
 using RoR2;
-using UnityEngine;
 
 namespace TotalItemCount
 {
@@ -28,11 +28,25 @@ namespace TotalItemCount
             orig(self, master);
 
             self.nameLabel.text = Util.GetBestMasterName(master) + " | " + GetItemCount(master);
+            self.moneyText.enableWordWrapping = true;
+        }
+
+        private void SetItemCountInMoney(
+            On.RoR2.UI.ScoreboardStrip.orig_Update orig,
+            RoR2.UI.ScoreboardStrip self
+        )
+        {
+            orig(self);
+
+            var master = self.GetFieldValue<CharacterMaster>("master");
+            self.moneyText.text = string.Format("{0} Items   ${1}", GetItemCount(master), master.money);
         }
 
         public void Awake()
         {
             On.RoR2.UI.ScoreboardStrip.SetMaster += SetItemCountDisplay;
+
+            On.RoR2.UI.ScoreboardStrip.Update += SetItemCountInMoney;
         }
 
         public void Destroy()
