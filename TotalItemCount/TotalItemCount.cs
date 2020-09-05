f64855b13e1eccff8e81de905ae65d0a00ee95b3
Amar1729 <amar.paul16@gmail.com>
2020-09-05 16:11:23 -0400

    refactor

--- TotalItemCount/TotalItemCount.cs
+++ TotalItemCount/TotalItemCount.cs
@@ -9,25 +9,35 @@ namespace TotalItemCount
     [BepInPlugin("com.theOneTrueGod.TotalItemCount", "Gives total item count", "1.0.0")]
     public class TotalItemCount : BaseUnityPlugin
     {
-        private int itemCount = 0;
-        private Transform HUDRoot = null;
-        public void Awake()
+        private int GetItemCount(CharacterMaster master)
         {
-            On.RoR2.UI.ScoreboardStrip.SetMaster += addTotalItemCount;
-        }
-
-        private void addTotalItemCount(On.RoR2.UI.ScoreboardStrip.orig_SetMaster orig, RoR2.UI.ScoreboardStrip self, CharacterMaster master)
-        {
-            orig(self, master);
-
-            Chat.AddMessage("hello the scoreboard is up");
             int itemCount = 0;
             for (ItemTier i = 0; i <= ItemTier.Boss; i++)
             {
                 itemCount += master.inventory.GetTotalItemCountOfTier(i);
             }
+            return itemCount;
+        }
 
-            self.nameLabel.text = Util.GetBestMasterName(master) + " | " + itemCount;
+        private void SetItemCountDisplay(
+            On.RoR2.UI.ScoreboardStrip.orig_SetMaster orig,
+            RoR2.UI.ScoreboardStrip self,
+            CharacterMaster master
+        )
+        {
+            orig(self, master);
+
+            self.nameLabel.text = Util.GetBestMasterName(master) + " | " + GetItemCount(master);
+        }
+
+        public void Awake()
+        {
+            On.RoR2.UI.ScoreboardStrip.SetMaster += SetItemCountDisplay;
+        }
+
+        public void Destroy()
+        {
+            On.RoR2.UI.ScoreboardStrip.SetMaster -= SetItemCountDisplay;
         }
     }
 }
